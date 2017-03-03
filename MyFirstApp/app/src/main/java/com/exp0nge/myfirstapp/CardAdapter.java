c02506b3748b9cf5465140d18926f0cf7cb0747a
MD Islam <mdislamwork@gmail.com>
2017-03-02 22:14:55 -0500

    radioactive explosion

---
+++ MyFirstApp/app/src/main/java/com/exp0nge/myfirstapp/CardAdapter.java
@@ -0,0 +1,13 @@
+package com.exp0nge.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_adapter);
+    }
+}
