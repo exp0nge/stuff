d30a2e97ce35675e93bd992ff560901341aa2f00
U-DESKTOP-3R4QKP1\exp0nge <md@appfigures.com>
2017-03-10 20:45:10 -0500

    add card view + adapter

--- MyFirstApp/app/src/main/java/com/exp0nge/myfirstapp/CardAdapter.java
+++ MyFirstApp/app/src/main/java/com/exp0nge/myfirstapp/CardAdapter.java
@@ -2,12 +2,50 @@ package com.exp0nge.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class CardAdapter extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
+    public List<String> mDataset;
+
+    public CardAdapter(List<String> mDataset) {
+        this.mDataset = mDataset;
+    }
+
+    public CardAdapter() {
+        mDataset = new ArrayList<>();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_card_adapter);
+    public CardAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(CardAdapter.ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextView = (TextView) itemView.findViewById(R.id.info_text);
+        }
     }
 }
--- MyFirstApp/app/src/main/java/com/exp0nge/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/exp0nge/myfirstapp/MainActivity.java
@@ -3,23 +3,37 @@ package com.exp0nge.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.exp0nge.myfirstapp.MESSAGE";
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private CardAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new CardAdapter();
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        mAdapter.mDataset.add(0, message);
+        mAdapter.notifyItemInserted(0);
     }
 }
